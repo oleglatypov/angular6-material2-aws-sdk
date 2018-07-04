bdd19299270f5fe2699feed52d8df26b45a1ecc7
oleglatypov <ovlatypov@gmail.com>
2018-07-04 05:21:56 -0400

    Some global edits
    Signed-off-by: oleglatypov <ovlatypov@gmail.com>

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { firebase } from '@firebase/app';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
